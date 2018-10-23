1b0ec1bbb53b5f5f3e5a267f8b8322c0686f72d2
Matthieu Borgognon <matrash@bqn.ch>
2018-10-23 14:25:27 +0200

    Filtering part without logic

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,47 @@ jQuery(function () {
     });
   });
 });
+
+
+
+$(function() {
+  $('input[name="daterange"]').daterangepicker({
+    "locale": {
+      "format": "DD/MM/YYYY",
+      "separator": " - ",
+      "applyLabel": "Apply",
+      "cancelLabel": "Cancel",
+      "fromLabel": "From",
+      "toLabel": "To",
+      "customRangeLabel": "Custom",
+      "weekLabel": "W",
+      "daysOfWeek": [
+          "Su",
+          "Mo",
+          "Tu",
+          "We",
+          "Th",
+          "Fr",
+          "Sa"
+      ],
+      "monthNames": [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+      ],
+      "firstDay": 1
+  },
+    opens: 'right'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
