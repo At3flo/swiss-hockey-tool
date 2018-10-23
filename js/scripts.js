ce367598fb57c3b359f512a4283727294263d95f
Matthieu Borgognon <matrash@bqn.ch>
2018-10-23 10:21:39 +0200

    Localization done with persistance

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,36 @@
-var update_texts = function() {
-
-    $('body').i18n();
-
+var set_locale_to = function (locale) {
+  if (locale) {
+    $.i18n().locale = locale;
+  }
+  $('body').i18n();
 };
 
-$( document ).ready(function() {
-
-  $.i18n().load( {
+jQuery(function () {
+  $.i18n().load({
     'en': 'i18n/en.json',
     'fr': 'i18n/fr.json',
     'de': 'i18n/de.json'
+  }).done(function () {
+    set_locale_to(url('?locale'));
+
+    History.Adapter.bind(window, 'statechange', function () {
+      set_locale_to(url('?locale'));
+    });
+
+    set_locale_to(localStorage.getItem("locale"));
+
+
+    $('.lang-switch').on('click', 'a', function (e) {
+      e.preventDefault();
+      History.pushState(null, null, "?locale=" + $(this).data('locale'));
+      localStorage.setItem("locale", $(this).data('locale'));
+    });
+
+    $('.lang-switch').click(function (e) {
+      e.preventDefault();
+      History.pushState(null, null, "?locale=" + $(this).data('locale'));
+      $.i18n().locale = $(this).data('locale');
+      $('body').i18n();
+    });
   });
-
-  $.i18n().locale = 'fr';
-
 });
-
-
-$('.lang-switch').click(function(e) {
-
-    e.preventDefault();
-
-    $.i18n().locale = $(this).data('locale');
-
-    update_texts();
-
-});
-
-
-
