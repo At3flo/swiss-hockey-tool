b80a76b23a468c3c83feafc8e21074fc0ad85c2c
Matthieu Borgognon <matrash@bqn.ch>
2019-06-27 15:22:30 +0200

    Removed some useless config lines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :set_locale
-  # before_action :set_locale
-
-  rescue_from ActionController::UnpermittedParameters do |error|
-    message = "Invalid parameter: %s. " % error.params.to_sentence
-    message << 'Please verify that the parameter name is valid and the values are the correct type.'
-    render_error 'param_error', :status => :bad_request, :locals => { :exception => error, :message => message }
-  end
 
   private
   
