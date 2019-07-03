0cea337bd492d32024161a59c25c2ca1394b4351
Matthieu Borgognon <matrash@bqn.ch>
2019-07-03 07:29:21 +0200

    Modified form update for places left and total places

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,7 +15,7 @@ class EventsController < ApplicationController
     categories_h = {}
     Category.all.each do |category|
       if params["category-#{category.id}"]
-        categories_h[category.id] = { "places_left": params["total_places-#{category.id}"], "total_places": params["total_places-#{category.id}"] }
+        categories_h[category.id] = { "places_left": params["total_places-#{category.id}"].to_i, "total_places": params["total_places-#{category.id}"].to_i }
       else 
         categories_h[category.id] = { "places_left": 0, "total_places": 0 }
       end
@@ -56,17 +56,24 @@ class EventsController < ApplicationController
     @event.date = Time.new(params[:event]["date(1i)"], params[:event]["date(2i)"], params[:event]["date(3i)"]).strftime("%F")
     @event.period = params[:event][:period]
     @event.start_time = Time.new(2000, 1, 1, params[:event]["start_time(4i)"], params[:event]["start_time(5i)"]).strftime("%H:%M")
-    @event.places_left += params[:event][:total_places].to_i - @event.total_places.to_i
-    @event.total_places = params[:event][:total_places].to_i
     @event.location = params[:event][:location]
     @event.contact = params[:event][:contact]
     @event.is_tournament_open = params[:event][:is_tournament_open]
     @event.is_outdoor = params[:event][:is_outdoor]
     @event.other_informations = params[:event][:other_informations]
     @event.user = current_user
-    @event.category = Category.find(params[:event][:category_id].to_i)
+
     
-    @event.is_tournament_open = false if @event.places_left == 0
+    categories_h = {}
+    Category.all.each do |category|
+      if params["category-#{category.id}"]
+        categories_h[category.id] = { "places_left": @event[:categories][category.id.to_s]["places_left"].to_i + (params["total_places-#{category.id}"].to_i - @event[:categories][category.id.to_s]["places_left"].to_i), "total_places": params["total_places-#{category.id}"].to_i }
+      else 
+        categories_h[category.id] = { "places_left": 0, "total_places": 0 }
+      end
+    end
+
+    @event.categories = categories_h
 
     if @event.save!
       redirect_to root_path
